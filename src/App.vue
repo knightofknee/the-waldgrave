63c8a942302801536965c9fe888e23ef56628034
knightofknee <bricarlis@gmail.com>
2020-07-16 21:48:04 -0500

    Adding initial files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img class="bigw" src="./assets/w.jpg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.bigw {
+  height: auto;
+  max-width: 500px;
+  margin: -10% 0;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Core Values</h2>
+    <ul>
+      <li>
+        <span>Creativity</span>
+      </li>
+      <li>
+        <span>Weird</span>
+      </li>
+      <li>
+        <span>Effort</span>
+      </li>
+    </ul>
+    <h2>Goal</h2>
+    <ul>
+      <li>
+        <p>We are creatures of creation and expression, but our everyday
+          lives do not always give us outlets, so we must create spaces that do.
+          The Waldgrave aims to provide the tools to create those spaces for you and your friends,
+          and to promote the creation and capturing of ideas. Our tools are aimed at both the
+          experienced creative and the person who lacks a current outlet.
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'The Waldgrave welcomes you'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
