c98fd9a5250513dcac110e724d2754d58f3625fc
knightofknee <bricarlis@gmail.com>
2021-06-26 02:01:25 -0500

    bebooop

--- src/components/CreateComment.vue
+++ src/components/CreateComment.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    <div>Create a comment</div>
-    <input type="text" />
-    <input type="number" />
+  <div style="display:flex;justify-content:space-evenly">
+    <form style="display:flex;flex-direction:column;align-items:flex-end;">Create a comment
+      <div>
+        <label name="author">Name</label>
+        <input type="text" name="author" />
+      </div>
+      <div>
+        <label name="pageNumber">Page Number</label>
+        <input type="number" name="pageNumber" />
+      </div>
+      <div>
+        <label name="comment">Comment</label>
+        <textarea style="width:100%;" rows="5" name="comment" />
+      </div>
+    </form>
   </div>
 </template>
 
