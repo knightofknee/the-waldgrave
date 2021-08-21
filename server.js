b444b42e0f8e4ca53c3662e89583b7cae5703d02
knightofknee <bricarlis@gmail.com>
2021-08-20 19:21:56 -0500

    maybenothing

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+const connectHistory = require('connect-history-api-fallback');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use(connectHistory());
 
 var port = process.env.PORT || 5000;
 app.listen(port);
