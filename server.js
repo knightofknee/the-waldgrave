9d4b9214dc8600204b10a79eb412a59447eb8c45
knightofknee <bricarlis@gmail.com>
2021-09-12 20:08:50 -0500

    gog

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const history = require('connect-history-api-fallback');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-app.get('/.well-known/acme-challenge/RhOO0oMxnE7HkNsxDhX5ldcdF4SIhMu3D0xte5XsoT4', (req, res) => {
-  res.send(`RhOO0oMxnE7HkNsxDhX5ldcdF4SIhMu3D0xte5XsoT4.qrcQEr-yX_-d1mMWDJ7MxPeZpMknbyClWtwLuisGXV4`)
-});
+// app.get('/.well-known/acme-challenge/RhOO0oMxnE7HkNsxDhX5ldcdF4SIhMu3D0xte5XsoT4', (req, res) => {
+//   res.send(`RhOO0oMxnE7HkNsxDhX5ldcdF4SIhMu3D0xte5XsoT4.qrcQEr-yX_-d1mMWDJ7MxPeZpMknbyClWtwLuisGXV4`)
+// });
 
 app.get('/test', (req, res) => {
   res.send(`wooo`)
