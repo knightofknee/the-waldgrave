e97d23debf2cb9552bc6cea19bc159314dd5370d
knightofknee <bricarlis@gmail.com>
2021-09-26 14:03:29 -0500

    gog

--- src/assets/TandSSongs.js
+++ src/assets/TandSSongs.js
@@ -1,3 +1,5 @@
+import { text } from "express"
+
 export default [
   {title: 'Hold My Breath Until I Die', lyrics: [`[Verse 1]
   Late at night, when your words are eating me alive
@@ -172,13 +174,462 @@ export default [
   I'll be back someday
   But I'll be back someday
   I'll be back someday`]},
-  { title: `Don’t Believe the Things They Tell You (They Lie)`, lyrics: [``]},
-  { title: `Hello, I’m Right Here`, lyrics: [``]},
-  { title: `I Don’t Owe You Anything`, lyrics: [``]},
-  { title: `I Know I’m Not the Only One`, lyrics: [``]},
-  { title: `Please Help Me`, lyrics: [``]},
-  { title: `Keep Them Close ’Cause They Will Fuck You Too`, lyrics: [``]},
-  { title: `We Don’t Have Fun When We’re Together Anymore`, lyrics: [``]},
-  { title: `You Go Away and I Don’t Mind`, lyrics: [``]},
-  { title: `All I Have to Give the World Is Me`, lyrics: [``]}
+  { title: `Don’t Believe the Things They Tell You (They Lie)`, lyrics: lyricsToArray(`[Verse 1]
+  The things I've almost told you
+  All the things I've thought about you
+  It takes all my strength to face you
+  To hold still
+  And when you give me your attention
+  Things get lost in translation, again
+  Tell you I don't love you at all
+  But I do love you
+
+  [Pre-Chorus]
+  I don't wanna be a liar
+  But I do it every day
+  I don't wanna be so tired
+  But I can't sleep any way
+
+  [Chorus]
+  Don't believe the things they tell you, they lie
+  Don't believe the things they tell you, no
+
+  [Verse 2]
+  The thing my mother told me
+  Was that everyone would love me
+  But in the dark, I feel so lonely
+  I'm numb
+  She said it with so much conviction
+  But I feel like something's missing, again
+  Ooh, I don't love me at all
+  I don't love me
+
+  [Pre-Chorus]
+  I don't wanna be a liar
+  But I do it every day
+  I don't wanna be so tired
+  But I can't sleep any way
+
+  [Chorus]
+  Don't believe the things they tell you, they lie
+  Don't believe the things they tell you, no
+
+  [Bridge]
+  I'm a liar
+  No one believe me
+  I'm a liar
+  No one believe me
+
+  [Chorus]
+  Don't believe the things they tell you
+  Don't believe the things they tell you
+  Don't believe the things I tell you, I lie
+  Don't believe the things I tell you, no`)},
+  { title: `Hello, I’m Right Here`, lyrics: lyricsToArray(`[Verse 1]
+  Right now, I feel so winded
+  Thinkin' that one day, we'll be ending
+
+  [Pre-Chorus]
+  Until I've done all that I can (All that)
+  Until I've done all that I can
+
+  [Chorus]
+  And so I said, "Hello, hello, hello, I'm right here"
+  Oh, "Hello, hello, hello, I'm right here"
+
+  [Verse 2]
+  Right now, I wish I was older
+  And right now, I wish I had closure
+
+  [Pre-Chorus]
+  Until I've done all that I can (All that)
+  Until I've done all that I can
+
+  [Chorus]
+  And so I said, "Hello, hello, hello, I'm right here"
+  Oh, "Hello, hello, hello, I'm right here"
+
+  [Bridge]
+  Drinkin' with nothing to lose
+  I'm sorry I was late, I was so blue
+  Oh, my head hurts from holding your hand
+  And I'm young, but it's hard to believe
+  Yeah, yeah
+  That someday, I will never see you again
+  And you might not believe this, but you've changed me so much
+  Oh, baby (Baby), it's so hard leaving you
+  But maybe (Maybe), this was the only, only way
+
+  [Chorus]
+  And so I said, "Hello, hello, hello, I'm right here"
+  Oh, "Hello, hello, hello, I'm right here"
+  "Hello, hello, hello, I'm right here"
+  "Hello, hello, hello, I'm right here"`)},
+  { title: `I Don’t Owe You Anything`, lyrics: lyricsToArray(`[Verse 1]
+  Here when you need it
+  Take it all like you do
+  My heart ain't fadin'
+  But it might fail you
+
+  [Pre-Chorus]
+  'Cause I don't have anything left
+  And I can't have faith in you anymore
+  You just walk out, make it look tough
+  Say you really don't feel you can take no more
+
+  [Chorus]
+  Baby, I don't ask much
+  All I want is your touch
+  It's the way I love you
+  It's why I hate you
+  But I don't owe you
+  No, I don't owe you anything
+  I don't owe you
+  I don't owe you anything
+
+  [Verse 2]
+  Oh, when your magic
+  Wears off, what then?
+  Will you forgive me
+  When I am done?
+
+  [Pre-Chorus]
+  'Cause I don't have anything left
+  And I don't have faith in you anymore
+  I just walk out, try to sound tough
+  Say I really feel that I can't take no more
+
+  [Chorus]
+  Baby, I don't ask much
+  All I want is your touch
+  It's the way I love you
+  It's why I hate you
+  But I don't owe you
+  No, I don't owe you anything
+  I don't owe you
+  I don't owe you anything
+
+  [Bridge]
+  I gave it all tonight
+  I gave up love tonight
+  I waited up all night
+  I can't be wrong
+  But I don't owe you
+  No, I don't owe you anything
+
+  [Chorus]
+  Baby, I don't ask much
+  All I want is your touch
+  It's the way I love you
+  It's why I hate you
+  But I don't owe you
+  No, I don't owe you anything
+  I don't owe you
+  I don't owe you anything`)},
+  { title: `I Know I’m Not the Only One`, lyrics: lyricsToArray(`[Verse 1]
+  Dragging you down with backflips
+  Do you even care?
+  Keep a secret for me
+  Something I can't share
+  I wonder if someday, we'll just be a memory
+  If we go off the track, will you wait for me?
+
+  [Chorus]
+  I know I'm not the only one who needs you like I do
+  I know I'm not the only one who loves you like I do
+  I wait, but when I get lonely
+  Your words won't make me stronger
+  The luck that brought you to me
+  Won't hold us any longer
+
+  [Verse 2]
+  You'll be good to me, but you're not my dream
+  You'll lose me to places, places I have never been
+  The way we were, the way that you held my hand
+  For you, I will try so hard, still, I will let you down
+
+  [Chorus]
+  You'll learn you're not the only one who needs me like you do
+  You'll learn you're not the only one who loves me like you do
+  You wait, but when you get lonely
+  My words won't make you stronger
+  The luck that brought you to me
+  Won't hold us any longer
+
+  [Bridge]
+  I know I'm not the only one
+  I know I'm not, I know I'm not
+  I know I'm not the only one
+
+  [Outro]
+  I'm dragging you down with backflips
+  I'm upside down`)},
+  { title: `Please Help Me`, lyrics: lyricsToArray(`[Verse 1]
+  What if I don't do the things I need?
+  What if I don't know who I can believe?
+  What if I become all the horrible things I said that I would never be?
+  Waste my life and then never see
+  Take and take, and I never give
+
+  [Refrain]
+  Oh, please help me
+  Oh, please help me
+
+  [Chorus]
+  I needed a place to rest my head
+  Needed a place to make my bed
+  Wanted something better than this
+  Needed a place to rest my head
+  All the things I'll never be (I'll never be)
+  Waste my life and never see (I never see)
+  Take and take, I never give (I never give)
+  All the things I'll never be
+
+  [Verse 2]
+  What if I don't feel like I belong?
+  What happens to my head if I go beyond?
+  What if I become all the horrible things I swore that I would never be?
+  Waste my life, I'm never free
+  Take and take 'til I'm empty
+
+  [Refrain]
+  Oh, please help me
+  Oh, please help me
+
+  [Chorus]
+  I needed a place to rest my head
+  Needed a place to make my bed
+  Wanted something better than this
+  Needed a place to rest my head
+  All the things I'll never be (I'll never be)
+  Waste my life and never see (I never see)
+  Take and take, I never give (I never give)
+  All the things I'll never be
+
+  [Bridge]
+  Who am I to disagree?
+  Who am I to fall apart?
+  I lived my life the best I could
+  Lived my life the best I could
+
+  [Chorus]
+  I needed a place to rest my head
+  Needed a place to make my bed
+  Wanted something better than this
+  Needed a place to rest my head
+  All the things I'll never be (I'll never be)
+  Waste my life and never see (I never see)
+  Take and take, I never give (I never give)
+  All the things I'll never be`)},
+  { title: `Keep Them Close ’Cause They Will Fuck You Too`, lyrics: lyricsToArray(`[Verse 1]
+  The problem with starting halfway through
+  Is that you don't understand and that, that gets to you
+  Don't need to hear your voice to know you're outside looking in
+  Don't need to see your face, I know you're inside out again
+
+  [Chorus]
+  Keep them close 'cause they will fuck you too
+  I will keep you close
+  I got nothing better to do
+  Better to do
+  'Cause I will miss you
+  And without you, I will never be whole (Never, never, never)
+  I'll never be full again
+  I'm missing you like I used to
+  Like I still do
+
+  [Verse 2]
+  Sitting here tonight, I'm thinking those simple thoughts
+  Hoping that you're happy now, happy with what ya got
+
+  [Chorus]
+  Keep them close 'cause they will fuck you too
+  I will keep you close
+  I got nothing better to do
+  Better to do
+  'Cause I will miss you
+  And without you, I will never be whole (Never, never, never)
+  I'll never be full again
+  I'm missing you like I used to
+  Like I still do
+
+  [Bridge]
+  Wise the fuck up, know that I'll never lie to you
+  I won't lie to you
+  Wise the fuck up, know that I'll never lie to you
+  You knew all along
+
+  [Chorus]
+  Keep them close 'cause they will fuck you too
+  I will keep you close
+  I got nothing better to do (Got nothing)
+  Better to do (Got nothing)
+
+  [Outro]
+  I'm missing you
+  I'm still missing you
+  I'm missing you
+  I'm still missing you
+  I'm missing you (Got nothing)
+  I'm still missing you (Got nothing)
+  I'm missing you
+  I'm still missing you`)},
+  { title: `We Don’t Have Fun When We’re Together Anymore`, lyrics: lyricsToArray(`[Verse 1]
+  Promised myself I wouldn't let you get that way this time
+  Promised myself I wouldn't let you lose yourself this time
+  What you gonna do tomorrow when you wake up in
+  Someone else's bed?
+  What you gonna do tomorrow when you wake up in
+  Someone else's head?
+
+  [Chorus]
+  We don't have fun when we're together anymore
+  All we get when we're together is bored
+  We don't have fun when we're together anymore
+  All we get when we're together
+  All we get when we're together
+
+  [Verse 2]
+  Watch my head, falls in your hands
+  I wonder where have I been all this time?
+  Watch my thoughts from in your dreams
+  I wonder where have you been all this time?
+  Have another drink, it'll make you cool
+  It'll make you you
+  Have another drink, it'll make you cool
+  It'll make you you (You, you, you, you)
+
+  [Chorus]
+  We don't have fun when we're together anymore
+  All we get when we're together is bored
+  We don't have fun when we're together anymore
+  All we get when we're together
+  All we get when we're together
+
+  [Bridge]
+  Another day, another waste
+  Close my eyes and fall behind
+  Another day, another waste
+  Close my eyes and fall behind
+
+  [Chorus]
+  We don't have fun when we're together anymore
+  All we get when we're together
+  Another day, another waste
+  Close my eyes and fall behind
+  Another day, another waste
+  Close my eyes and fall behind
+
+  [Outro]
+  Close my eyes and fall behind
+  Close my eyes and fall behind`)},
+  { title: `You Go Away and I Don’t Mind`, lyrics: lyricsToArray(`[Verse 1]
+  I'm complicated, I was raised by icons
+  Touched by saints, still powerless
+  Scared and lost, cursed like novelty
+  Crushed and cracked, I'm powerless
+
+  [Pre-Chorus]
+  Hey yeah yeah, hey yeah yeah
+  Hey yeah yeah, hey yeah yeah
+
+  [Chorus]
+  Oh yeah, you go away, go away
+  And I don't mind
+  Yeah, you go away, go away
+  Well, I'll be fine
+  Bye, bye, bye, I still want you
+  Bye, bye, bye, I still need you
+
+  [Verse 2]
+  Instamatic, embraced by millions
+  A slow reaction hits my blood
+  If I get famous, inflamed by gravity
+  Pretend to notice, kiss my spills
+
+  [Pre-Chorus]
+  Hey yeah yeah, hey yeah yeah
+  Hey yeah yeah, hey yeah yeah
+
+  [Chorus]
+  Oh yeah, you go away, go away
+  And I don't mind (And I don't mind)
+  Yeah, you go away, go away
+  Well, I'll be fine (Well, I'll be fine)
+  Oh yeah, you go away, go away
+  And I don't mind (And I don't mind)
+  Yeah, you go away, go away
+  Well, I'll be fine (Well, I'll be fine)
+  Bye, bye, bye, I still want you
+  Bye, bye, bye, I still need you
+
+  [Bridge]
+  It's your mind that matters most
+  It's your mind that makes you mine
+  It's your mind that makes you mine
+  It's your mind that makes you mine
+
+  [Outro]
+  Go away, go away, I don't mind
+  Go away, go away, I'll be fine
+  Bye, bye, bye, I know I'll miss you`)},
+  { title: `All I Have to Give the World Is Me`, lyrics: lyricsToArray(`[Verse 1]
+  Faced myself in the mirror last night
+  I looked for things you said you liked
+  The person I am when no one's around
+  The girl I don't want to be found
+  Don't you ever wanna change?
+  Don't you ever wanna change?
+  Talked myself outta being me
+  Didn't wanna fight my own worst enemy
+  Wipe me out, did it all from view
+  Destroyed myself, became more like you
+  No one's very real today
+  No one's very real today
+
+  [Chorus]
+  All I have to give this world is me, and that's it
+  All I have to show this world is me, and that's it
+  Just me, just me, just me
+  And that's it
+
+  [Verse 2]
+  Convinced myself not to be so strange
+  I hope I still take your breath away
+  I look in the mirror, I see the face
+  The one who worries it can't change
+  You don't have to change, you don't have to change
+  You don't have to change
+
+  [Chorus]
+  All I have to give this world is me, and that's it
+  All I have to show this world is me, and that's it
+  Just me, just me, just me
+  And that's it
+
+  [Bridge]
+  Staring down my two halves in the glass
+  Feels like a heart attack
+  Telling me to choose one or the other
+  I get so panicked
+  I don't wanna lose, but if I choose love
+  What will happen then?
+  Staring down my two halves in the glass
+  What then?
+  Go ahead and choose, go ahead and choose
+  Go ahead and choose
+
+  [Chorus]
+  All I have to give this world is me, and that's it
+  All I have to show this world is me, and that's it
+
+  [Outro]
+  I don't wanna lose, but if I choose love
+  What will happen then?
+  Staring down my two halves in the glass
+  What then?`)}
 ]
+
+function lyricsToArray(text){
+  return text.split('\n\n')
+}
