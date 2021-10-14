fb8314e915b939107e5d59b52d3ec889e465f8ed
knightofknee <bricarlis@gmail.com>
2021-10-13 22:45:21 -0500

    gog

--- server.js
+++ server.js
@@ -6,17 +6,6 @@ const history = require('connect-history-api-fallback');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-      if (req.headers['x-forwarded-proto'] !== 'https')
-          // the statement for performing our redirection
-          return res.redirect('https://' + req.headers.host + req.url);
-      else
-          return next();
-  } else
-      return next();
-});
-
 var port = process.env.PORT || 5000;
 app.use(history()).listen(port);
 console.log('server started '+ port);
