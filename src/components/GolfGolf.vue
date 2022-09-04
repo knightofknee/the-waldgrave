ce67ae0555d110952d9704b09ed232647575a749
knightofknee <bricarlis@gmail.com>
2022-09-04 16:55:20 -0500

    gog

---
+++ src/components/GolfGolf.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="newOuterShell" style="display:flex;justify-content:center;align-items:center;">
+  <p>
+    GolfGolf mini-golf. All about the golf, profit capped at 1%. Over is used to cover costs of upgrades and to lower future prices. Every hole is a banger, both a unique golf challenge and unique theme. Black curtains between. It is byob and has a bar with at-cost+1$ beers and wine. Bartender can charge whatever they want for other things. Every hole has a target you can hit that gives you -1, making a perfect score near impossible. Highest level of investment: you never really wait because you spend set time on holes just watching what happens while you are there. Someone could be changing the hole while you are on it, like a an avalanche or slowly cracking ice. Profit is capped but costs are not. The art staff can choose whatever they find best and push barriers. They are mini-golf's core, the creators. (We could have a burlesque night where dancers dance among the holes, both fun and edgy for publicity).
+  </p>
+</div>
+</template>
+
+<script>
+
+// strats:
+// the quotes get in the way, need to refer to them as you scroll, +  a lock lyrics functionality
+
+export default {
+  name: 'GolfGolf',
+  props: { },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+p {
+  margin: 0 5%;
+}
+
+iframe {
+  margin: 2% 0;
+}
+</style>
