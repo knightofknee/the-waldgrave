d5eeab8f3235af5c10f19cb4de7914b9de3a2442
knightofknee <bricarlis@gmail.com>
2023-01-09 22:24:40 -0600

    gog

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var enforce = require('express-sslify');
+var http = require('http');
 const history = require('connect-history-api-fallback');
 
 app = express();
+
+app.use(enforce.HTTPS());
+
+
+
 app.use(serveStatic(__dirname + "/dist"));
 
-app.enable('trust proxy')
+// app.enable('trust proxy')
 
-app.use(function(request, response, next) {
+// app.use(function(request, response, next) {
 
-  if (process.env.NODE_ENV != 'development' && !request.secure) {
-     return response.redirect("https://" + request.headers.host + request.url);
-  }
-  next();
-})
+//   if (process.env.NODE_ENV != 'development' && !request.secure) {
+//      return response.redirect("https://" + request.headers.host + request.url);
+//   }
+//   next();
+// })
+app.use(history())
 
 var port = process.env.PORT || 5000;
-app.use(history()).listen(port);
-console.log('server started '+ port);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+// app.use(history()).listen(port);
+
+// console.log('server started '+ port);
