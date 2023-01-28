247577e1afaaa5355507a3d495065a37fac3949f
knightofknee <bricarlis@gmail.com>
2023-01-28 13:07:54 -0500

    gog

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,6 +21,39 @@
     <!-- <div class="purple-circle-container">
       <router-link to="/thepurplecircle"><img class="purple-circle" src="../assets/thepurplecircle.jpg"></router-link>
     </div> -->
+    <h2>2023 Update 8)</h2>
+    <div>
+      <p>
+        In 2022 I focused on improving my habits and designing them around my knowledge of neuroscience and psychology. I started the year targeting 7 daily habits (including this website I'm up to 8). My concept is I want to fill my day up with the things I want to do long term. I am also working on a vice tracking model, but I've been primarily concerned with filling time with positive actions. Here is where they stand:
+      </p>
+      <p>
+        Meditation is my biggest success so far. I had a 10 minute daily practice going at the beginning of the year. At the end of May I bumped it up to 20 minutes. On June 19th, I read <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5261734/">a neuroscience study</a> of people who have meditated over 10,000 hours. Their brain scans show unique activity, constant gamma brain wave activity (the fastest type of brain wave), and improvement in areas like empathy, which is supercharged. Basically, Nirvana is real, so since then I've been meditating an hour a day so I can reach Nirvana sometime before 2050.
+      </p>
+      <p>
+        Walking. I was ahead of my goal of 10,000 steps/day until the December cold and the holiday parties left me short. That is a win for me, my goal in walking tracking is to make sure I am hitting a minimum level of activity every day. In 2023, I want to keep up that minimum without caring about the number of steps listed in my phone. I spent too much time caring about it. I want to take at least one walk a day and only check my steps to see if I am below 6,000 for the day.
+      </p>
+      <p>
+        Reading. I've done a lot here but have far to go. I've been somewhat consistent, I made steady progress on my neuroscience book. For that, I'll set a timer for 10 minutes and aim for reading at least 2 pages. I scatter books around my house but still reach for my computer too often. I've begun making it a bedtime routine with some success (only fun books at night). My highlights for the year are the first Wheel of Time book, and Neal Stephenson's Quicksilver (it takes place during the Glorious Revolution and it is oddly accurate, what a weird time).
+      </p>
+      <p>
+        Yoga. I had some strong yoga runs, on the year I think I hit over 50% of days? I'll need my app to calculate that for me. I feel strong here, I am still getting a lot out of my 10-18 minute routine. I'm going gradually deeper into my stretches, so I feel great about where I am at with this one. Daily yoga makes such a massive difference.
+      </p>
+      <p>
+        Exercise. My performance here has been pretty weak overall, but strong at times. I had some stretches of strong cardio work when it was warm, and I have a jump rope and row machine now to help me in the winter. I'm optimistic about the jump rope, and am slowly getting used to it. At the end of the year I started doing my weighted pullups routine again, I am hoping to actually get into lifting this year. At the end of 2023, I want to be doing 50 pullups with 50 pounds of weight at least once a week. (Currenly I am at 32 pounds and manage 25 with, 25 without).
+      </p>
+      <p>
+        Creative Writing. I've been very consistent with this, any notes I take in my phone count. But I haven't gone deep at all. I have exercises to try out. This year I want to try writing longer things, like screenwriting or longer versions of my sci fi or fan fic ideas. I want to start holding regular rap camps, which will just be some friends coming over for brunch alongside trying out group writing exercises.
+      </p>
+      <p>
+        Music. This has mostly been piano for me, along with some singing and rap practice. I'll do 10-15 minutes of exercies then go through my piano book. After that I love to freestyle, slamming the keys. In 2023 I'm trying out recording myself more often. The inspiration is that when I rap freestyle, there is the desire to write down any banger lines, but that interrupts the flow. So now I occasionally turn on my mic and record the piano as well. It's been weird so far but it's helpful.
+      </p>
+      <p>
+        Working on the Waldgrave. So editing this site mainly. I've been focused on creating content, not showing it. So this year I hope to clean up the site so it can properly host the content I will be publishing. This is by far my least consistent item.
+      </p>
+      <p>
+        I have a weekly habit that I have neglected called 'Monday Night Review'. Here I look at all notes of the past week and select the ones that seem interesting. Before I have elaborated on those ideas or shared them with a friend. I am considering doing it as a youtube series and including them here.
+      </p>
+    </div>
     <h2>Goals</h2>
         <!-- We are creatures of creation and expression, but our everyday
           lives do not always give us outlets, so we must create spaces that do.
