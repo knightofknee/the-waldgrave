83d9f5b0369c1c6e4462edc556900f93e1476ed1
knightofknee <bricarlis@gmail.com>
2023-01-30 13:50:15 -0500

    gog

---
+++ src/components/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Nav Bar</h2>
+    <nav>
+      <router-link class="spacing" v-for="routes in links"
+      v-bind:key="routes.id"
+      :to="`${routes.page}`">{{routes.text}}</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: 'Home',
+          page:'/Home'
+        },
+        {
+          id: 1,
+          text: 'Contact',
+          page:'/Contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+
+</style>
